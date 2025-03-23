38b3788573fcfe8665e46127d5e3f0520ea2e447
davis <test@tt.com>
2025-03-23 21:30:11 +0800

    add the db sql

---
+++ db.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE role_resources (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    role VARCHAR(255) NOT NULL,
+    resource_url VARCHAR(255) NOT NULL
+);
